4de26e43c802f223e2fe8e088b3310a5cdc3b6ff
Gora Kong <gora.kong93@gmail.com>
2018-11-05 16:25:06 -0800

    last bit of styling for today

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,19 +3,26 @@ import PropTypes from 'prop-types';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import styles from './Header.css';
 
 library.add(faStar);
 
+function addStars(overallRating) {
+  const result = [];
+  for (let i = 0; i < 5; i += 1) {
+    result.push(<FontAwesomeIcon icon="star" />);
+  }
+  return result;
+}
+
 const Header = ({ numReviews, overallRating }) => (
-  <div>
-    <h2>{numReviews} Reviews</h2>
-    <FontAwesomeIcon icon="star" />
-    <FontAwesomeIcon icon="star" />
-    <FontAwesomeIcon icon="star" />
-    <FontAwesomeIcon icon="star" />
-    <FontAwesomeIcon icon="star" />
-    {overallRating}
-    <input type="text" placeholder="Search reviews" name="search" />
+  <div className="row">
+    <div className="col-md-6">
+      <h2>{numReviews} Reviews {addStars(overallRating)}</h2>
+    </div>
+    <div className="col-md-3 offset-md-3">
+      <h2><input type="text" placeholder="Search reviews" name="search" className={styles.search} /></h2>
+    </div>
   </div>
 );
 
