bbc2a81da24355b206532f0758cb4add50012123
Han Lin Aung <hanlinaunghack@gmail.com>
2018-11-14 14:13:52 -0800

    fixed typos

--- server/app.js
+++ server/app.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const path = require('path');
+const path = require("path");
 
-const cors = require('cors');
+const cors = require("cors");
 
-const fetchAll = require('./model/index.js');
+const fetchAll = require("./model/index.js");
 
-app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use(express.static(path.join(__dirname, "/../client/dist")));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/api/rooms/:listingId/reviews', (req, res) => {
+app.get("/api/rooms/:listingId/reviews", (req, res) => {
   const id = req.params.listingId;
   fetchAll(id, (error, data) => {
     if (error) {
